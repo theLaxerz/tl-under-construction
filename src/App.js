dc5b39d4fb727cdc03346248de59d9b4ab887e2d
Aaron F. Lax <aaron.lax@icloud.com>
2020-03-11 18:36:59 -0500

    Added my Logo need to update the Flex and CSS.

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </div>
+        <div className="title">
+          theLaxerz 
+        </div>
+        <div className="headerItems">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          </div>
       </header>
     </div>
   );
