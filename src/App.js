4ca0d785373ed22f207526f7010f63535b249108
Aaron F. Lax <aaron.lax@icloud.com>
2020-03-12 08:26:54 -0500

    Fixed the layout of the Flex in the AppHeader and Changed to Roboto.

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
         </div>
         {/* HeaderItems */}
         <div className="block2">
-          <p>
+          <p className="block2">
             IT Consultants, Developers, Security Specialists
           </p>
-          <p>Linux/Windows Server Administration, System Architecture,</p>
-          <p>and all other needs <a href="mailto:support@thelaxerz.us"><code>support@thelaxerz.us</code></a></p>
+          <p className="block2">Linux/Windows Server Administration, System Architecture,</p>
+          <p className="block2">and all other needs <a href="mailto:support@thelaxerz.us"><code>support@thelaxerz.us</code></a></p>
         </div>    
       </header>
-     
+      
   );
 }
 
